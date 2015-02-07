7ada27432b1c6b3d34e0d5d4a35e1c3de74603ef
Spencer Barton <sbarton272@gmail.com>
2015-02-06 22:30:21 -0500

    Working on sprite test, need to update library

---
+++ spritelyTest/js/birdDemo.js
@@ -0,0 +1,30 @@
+// Bird Demo
+
+var $bird = $('<div/>', {
+	'id': 'bird',
+    'background': 'transparent url(img/bird.png) 0 0 no-repeat',
+    'position': 'absolute',
+    'top': '150px',
+    'left': '65px',
+    'width': '180px',
+    'height': '123px',
+    'z-index': '2000',
+    'cursor': 'pointer'
+});
+$('body').append($bird);
+
+
+console.log($('#bird'));
+console.log($bird);
+
+$('#bird').sprite({fps: 12, no_of_frames: 3})
+    .spRandom({
+        top: 70,
+        left: 100,
+        right: 200,
+        bottom: 340,
+        speed: 4000,
+        pause: 3000
+});
+
+console.log('Injected bird');

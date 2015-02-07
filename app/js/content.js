d6183253d6a044fe4f302b042439845016f2dbd9
billy <wtwood@andrew.cmu.edu>
2015-02-07 00:02:01 -0500

    Bwowoohahwwwa -- That's what I think the current music sounds like.

--- app/js/content.js
+++ app/js/content.js
@@ -1,8 +1,11 @@
-var myid = chrome.runtime.id;
-var myimg = "chrome-extension://" + myid + "/resources/img/test1.jpeg";
-var images = document.getElementsByTagName('img');
-console.log(myimg);
-for (var i = 0; i < images.length; i++) {
-  images[i].src = myimg;
+function doThing() {
+  var myid = chrome.runtime.id;
+  var myimg = "chrome-extension://" + myid + "/resources/img/test1.jpeg";
+  var images = document.getElementsByTagName('img');
+  var changeIdx = Math.floor(Math.random() * images.length);
+  images[changeIdx].src = myimg;
 }
 
+var timeout = Math.floor(Math.random() * 3000);
+jQuery("#tem_forma").hide();
+setTimeout( function() { doThing(); }, timeout);

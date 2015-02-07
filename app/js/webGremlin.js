f14d13fa5cc958d23e8fc5f176a6073d87466f87
billy <wtwood@andrew.cmu.edu>
2015-02-07 11:47:26 -0500

    Basically the best changes

--- app/js/webGremlin.js
+++ app/js/webGremlin.js
@@ -1,17 +1,15 @@
 /* Web Gremlin
- * Tartan Hacks 2015
- * CMU
- * Carson, Adam, Billy and Spencer
- */
+* Tartan Hacks 2015
+* CMU
+* Carson, Adam, Billy and Spencer
+*/
 
 //---------------------------------------------
 // Gremlin engine
 //---------------------------------------------
 
 var WebGremlin = {
-
-	aE: AnimationEngine,
-
+    aE: AnimationEngine,
 };
 
 //---------------------------------------------
@@ -19,39 +17,57 @@ var WebGremlin = {
 //---------------------------------------------
 
 var AnimationEngine = {
-	// Defined animations
-	MOVEMENT: "ANIMATION_MOVE",
+    // Defined animations
+    MOVEMENT: "ANIMATION_MOVE",
 
-	// Animation object used to play animation 
-	animate: function(animation) {
+    // Animation object used to play animation 
+    animate: function(animation) {
 
-		// Case on animation type
-		switch(animation.type) {
-			case MOVEMENT:
-				this.runMove(animation);
-				break;
-			default:
-				console.log('Unrecognized animation type ' + animation.type);
-				break;
-		}
-	},
+         // Case on animation type
+         switch(animation.type) {
+            case MOVEMENT:
+                this.runMove(animation);
+                break;
+            default:
+                console.log('Unrecognized animation type '+animation.type);
+                break;
+        }
+    },
 
-	// TODO create div for sprite, add sprite to body
-	// TODO animate sprite (per animation)
+    // TODO create div for sprite, add sprite to body
+    // TODO animate sprite (per animation)
 
-	//----------- Animations -----------------------
+    //----------- Animations -----------------------
+    // animation needs:
+    // - url
+    // - width
+    // - height
+    // - no_of_frames
 
-	// In place animation
-	runInPlace: function(animation) {
-		// TODO
-	},
+    // In place animation
+    runInPlace: function(animation) {
+        var topPerc = Math.floor(Math.random() * 80) + 10;
+        var leftPerc = Math.floor(Math.random() * 80) + 10;
+        var $sprite = $('<div/>', {
+            'id':'gremline',
+        })
+        .css({
+            'position':'absolute',
+            'top':String(topPerc) + '%',
+            'left':String(leftPerc) + '%',
+            'background-image':animtion.url,
+            'background-repeat':'no-repeat',
+            'background-color':'transparent',
+            'z-index':'10000'
+        });
+        $('body').append($sprite);
+        $sprite.sprite({fps: 12, no_of_frames: animation.no_of_frames});
+    },
 
-	// Move across screen in straight line
-	runMove: function(animation) {
-		console.log(MOVEMENT);
-	}
-
-
+    // Move across screen in straight line
+    runMove: function(animation) {
+        console.log(MOVEMENT);
+    }
 };
 
 //---------------------------------------------

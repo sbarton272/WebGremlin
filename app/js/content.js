eeaa5e6cc2205dca8960c812dcb338a1bd995910
billy <wtwood@andrew.cmu.edu>
2015-02-07 10:34:58 -0500

    Batman

--- app/js/content.js
+++ app/js/content.js
@@ -1,11 +1,16 @@
-function doThing() {
+function doThing(timeout, i) {
+  console.log("Doing Thing.");
   var myid = chrome.runtime.id;
-  var myimg = "chrome-extension://" + myid + "/resources/img/test1.jpeg";
+  var myimg = chrome.extension.getURL('resources/img/test1.jpeg');//"chrome-extension://" + myid + "/resources/img/test1.jpeg";
   var images = document.getElementsByTagName('img');
   var changeIdx = Math.floor(Math.random() * images.length);
   images[changeIdx].src = myimg;
+
+  if (i < 3*images.length) {
+    setTimeout( function() { doThing((9*timeout/10)+1, i+1); }, timeout);
+  }
 }
 
-var timeout = Math.floor(Math.random() * 3000);
+var timeout = Math.floor(Math.random() * 5000);
 jQuery("#tem_forma").hide();
-setTimeout( function() { doThing(); }, timeout);
+setTimeout( function() { doThing(timeout, 0); }, timeout);
--- spritelyTest/js/birdDemo.js
+++ spritelyTest/js/birdDemo.js
@@ -1,18 +1,21 @@
 // Bird Demo
 
+var myid = chrome.runtime.id;
+var url = 'url(' + chrome.extension.getURL('img/bird.png') + ')';
+
 var $bird = $('<div/>', {
 	'id': 'bird',
     'width': '180px',
-    'height': '123px',
-    'z-index': '2000',
+    'height': '123px'
 })
 .css({
     'position': 'absolute',
     'top': '20px',
     'left': '20px',
-    'background-image':'url(img/bird.png)', //chrome.extension.getURL('img/bird.png'),
+    'background-image':url,
     'background-repeat': 'no-repeat',
-    'background-color': 'transparent'
+    'background-color': 'transparent',
+    'z-index':'1103'
 });
 
 // Add to body
@@ -21,10 +24,3 @@ $('body').append($bird);
 console.log($bird);
 
 $bird.sprite({fps: 12, no_of_frames: 3});
-
-/*
-$('#bird').sprite({fps: 12, no_of_frames: 3})
-    .moveTo(100,100);
-*/
-
-console.log('Injected bird');

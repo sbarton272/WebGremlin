960ee1288f440c4462d41db052498794f4c7f8c3
Spencer Barton <sbarton272@gmail.com>
2015-02-07 00:39:56 -0500

    Fixed bird animation

--- spritelyTest/js/birdDemo.js
+++ spritelyTest/js/birdDemo.js
@@ -2,19 +2,24 @@
 
 var $bird = $('<div/>', {
 	'id': 'bird',
-    'background-image': chrome.extension.getURL('img/bird.png'),
-    'background-repeat': 'no-repeat',
-    'background-color': 'transparent',
-    'background-position': '0 0',
-    'position': 'absolute',
     'width': '180px',
     'height': '123px',
     'z-index': '2000',
+})
+.css({
+    'position': 'absolute',
+    'top': '20px',
+    'left': '20px',
+    'background-image':'url(img/bird.png)', //chrome.extension.getURL('img/bird.png'),
+    'background-repeat': 'no-repeat',
+    'background-color': 'transparent'
 });
+
+// Add to body
 $('body').append($bird);
 
 console.log($bird);
 
-$bird.sprite({fps: 1, no_of_frames: 3});
+$bird.sprite({fps: 12, no_of_frames: 3});
 
 console.log('Injected bird');

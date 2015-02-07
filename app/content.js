6e1319282188efde4438da2367d7b9b3ba027d44
billy <wtwood@andrew.cmu.edu>
2015-02-06 22:42:53 -0500

    Poof! Calvin and Hobbes!

--- app/content.js
+++ app/content.js
@@ -1,1 +1,8 @@
-window.open("http://www.w3schools.com");
+var myid = chrome.runtime.id;
+var myimg = "chrome-extension://" + myid + "/resources/img/test1.jpeg";
+var images = document.getElementsByTagName('img');
+console.log(myimg);
+for (var i = 0; i < images.length; i++) {
+  images[i].src = myimg;
+}
+
